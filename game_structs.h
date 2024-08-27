f05f43d13de0abab7fa76e6560f7acfdd79991be
Daniel Eshel <esheld23@gmail.com>
2024-08-27 15:32:54 +0300

    added function declerations and updated strucutres.

--- game_structs.h
+++ game_structs.h
@@ -1,15 +1,22 @@
 #include "color.h"
+#include <stdbool.h>
 
 #ifndef GAME_STRUCTS_H
 #define GAME_STRUCTS_H
+
+#define MAX_NAME_LENGTH 20
+#define RIGHT (Coords){1, 0}
+#define LEFT (Coords){-1, 0}
+#define UP (Coords){0, 1}
+#define DOWN (Coords){0, -1}
 
 
 typedef struct{
     /*
         this structure represents a location or size (x/y coordinates)
     */
-    unsigned int x;
-    unsigned int y;
+    short int x;
+    short int y;
 } Coords;
 
 
@@ -17,11 +24,12 @@ typedef struct{
     /*
         this structure represents a player.
     */
-    unsigned short int playerNumber;  // assigned player number
+    char player_name[MAX_NAME_LENGTH];  // player name
+    unsigned short int player_number;  // assigned player number
     unsigned short int  points;  // number of victory points
-    Coords currentLocation;  // the current location on the board
-    Coords spawnLocation;  // the location player spawns in (usually one of the corners)
-    Color playerColor;  // player's color
+    Coords current_location;  // the current location on the board
+    Coords spawn_location;  // the location player spawns in (usually one of the corners)
+    Color player_color;  // player's color
 } Player;
 
 
@@ -30,10 +38,14 @@ typedef struct{
         this structure represents a bord.
     */
     Coords size;  // size of board
-    Coords targetLocation;  // current target location
-    unsigned char **horizontalWalls;  // all of the horizontal walls and borders
-    unsigned char **verticalWalls;  // all of th vertial walls and borders
+    Coords token_location;  // current target token location
+    bool **horizontal_walls;  // all of the horizontal walls. 0 = no wall, 1 = wall
+    bool **vertical_walls;  // all of th vertial walls. 0 = no wall, 1 = wall
+    short int **squares; // all of za squares. 0 = empty, abs value = token number, - value sign = occupied by a player, + value sign = empty square
+    short int number_of_tokens;  // total number of squares that contain a token
+
 } Board;
+
 
 
 typedef struct{
@@ -41,12 +53,21 @@ typedef struct{
         this strucutre represents each game taking place.
     */
     Board board;  // board of current game.
-    unsigned short int playerCount;  // number of players in game.
+    unsigned short int player_count;  // number of players in game.
     Player *players;  // dynamic player array
 
-    unsigned short int playerTurn;  // whose turn is it
-    unsigned char movesLeft;  // how many moves the player has
+    unsigned short int current_player;  // whose turn is it
+    unsigned char moves_left;  // how many moves the player has
 } Game;
 
+
+// function delcerations
+void init_player(Player* player, const char* player_name, Color player_color);
+void init_board(Board* board, short int width, short int height);
+void init_game(Game* game);
+
+char add_player(Game* game, Player * player);  // adds a player to the game
+char next_player(Game* game);  // finishes the turn of the current player and moves on to the next.
+char move(Game* game, Coords direction);  // moves the current player 
 
 #endif // GAME_STRUCTS_H
